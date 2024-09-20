446296aad4b5d24555229410fb6a30b703ce9067
SK1PPR <agrawalkhushal04@gmail.com>
2024-09-20 19:22:09 +0530

    First commit

---
+++ survivor_game.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+func spawn_mob():
+	var new_mob = preload("res://mob.tscn").instantiate()
+	%PathFollow2D.progress_ratio = randf()
+	new_mob.global_position = %PathFollow2D.global_position
+	add_child(new_mob)
+
+
+func _on_timer_timeout():
+	spawn_mob()
+
+
+func _on_player_health_deplete():
+	%GameOverScreen.visible = true
+	get_tree().paused = true
