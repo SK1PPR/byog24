cca6ebc22a29fb36252e072d71249c3fa37b13cf
Darkhood148 <ujjwal.sharma9999999@gmail.com>
2024-09-22 07:58:46 +0530

    random cards order

--- data/Powerups/powerup_base.gd
+++ data/Powerups/powerup_base.gd
@@ -3,3 +3,4 @@ extends Resource
 
 @export var icon: Resource
 @export var ind: int
+@export var desc: String
--- scripts/powerup_manager.gd
+++ scripts/powerup_manager.gd
@@ -10,10 +10,13 @@ func execute_powerup() -> void:
 	assert(false, "Implement this in child class")
 	
 func spawn_random_pickup(pos: Vector2):
-	print('Random pickup lmfaooo: ', pos)
 	var chosenOne: PowerupBase = Powerups.pick_random()
 	var inst = pickupInst.instantiate()
 	inst.get_child(2).texture = chosenOne.icon
 	inst.callBackFunction = Callable(%PowerupManager.get_child(chosenOne.ind), "execute")
 	inst.global_position = pos
 	get_parent().add_child(inst)
+
+func generate_cards():
+	Powerups.shuffle()
+	$"../CardSelect/ColorRect/Card_Box".set_cards(Powerups[0], Powerups[1], Powerups[2])
