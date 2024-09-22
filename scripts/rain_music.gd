92021d8ac58b257beea2f1978510ead68b4a9f08
SK1PPR <agrawalkhushal04@gmail.com>
2024-09-22 05:44:01 +0530

    Main menu completed

---
+++ scripts/rain_music.gd
@@ -0,0 +1,16 @@
+extends Node
+
+func play_music():
+	$Rain.play()
+	
+func stop_music():
+	$Rain.stop()
+
+func lower_volume():
+	$Rain.volume_db -= 5
+
+func start_bg():
+	$Metal.play()
+
+func stop_rain():
+	$CPUParticles2D.emitting = false
