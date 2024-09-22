cdddd594a222f04f29cd96e5ff6583579d58f5c4
varun-r-mallya <varunrmallya@gmail.com>
2024-09-22 07:04:24 +0530

    sexy candles

---
+++ scripts/candle.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@export var noise: NoiseTexture2D
+var time_passed := 0.0
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	time_passed += delta
+	var sampled_noise = noise.noise.get_noise_1d(time_passed)
+	sampled_noise = abs(sampled_noise)*10
+	$".".energy = sampled_noise
